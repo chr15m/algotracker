24653db67d47573d35893a9b3d3c499e19640ec2
Chris McCormick <chris@mccormick.cx>
2022-11-19 21:34:54 +0800

    Generate mod binary from json definition.

--- src/algotracker/ui.cljs
+++ src/algotracker/ui.cljs
@@ -1,20 +1,33 @@
 (ns algotracker.ui
   (:require [reagent.core :as r]
-            [reagent.dom :as rdom]))
+            [reagent.dom :as rdom]
+            ["txt-tracker/savers/mod" :as save-mod]
+            [shadow.resource :as rc]))
 
 (defonce state (r/atom {}))
 
-(defn button-clicked [ev]
-  (swap! state update-in [:number] inc))
+(defn fix-pcm [mod-data]
+  (.map (aget mod-data "samples")
+        (fn [sample]
+          (js/console.log (aget sample "name"))
+          (let [pcm (aget sample "pcm")]
+            (when pcm
+              (aset pcm "length" (aget sample "length"))))))
+  mod-data)
 
-(defn component-main [state]
-  [:div
-   [:h1 "algotracker"]
-   [:p "Welcome to the app!"]
-   [:button {:on-click button-clicked} "click me"]
-   [:pre (pr-str @state)]  
-   [:p [:a {:href "/mypage"} "Static server rendered page."]]
-   [:p [:a {:href "/api/example.json"} "JSON API example."]]])
+(defn component-main [_state]
+  (let [mod-data (-> (rc/inline "2s-library.mod.json")
+                   js/JSON.parse
+                   fix-pcm
+                   save-mod)
+        mod-file (js/File. #js [mod-data] #js {:name "test.mod" :content-type "audio/x-mod"})]
+    [:div
+     [:h1 "algotracker"]
+     [:p "Algorithmically generating tracker music."]
+     [:a {:href (js/URL.createObjectURL mod-file) :download "test.mod"} [:button "download generated mod"]]
+     ;[:pre (pr-str @state)]
+     #_ [:p [:a {:href "/mypage"} "Static server rendered page."]]
+     #_ [:p [:a {:href "/api/example.json"} "JSON API example."]]]))
 
 (defn start {:dev/after-load true} []
   (rdom/render [component-main state]
