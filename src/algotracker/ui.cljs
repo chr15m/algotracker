99c335554f69996bcd71011b122a06516e0eb496
Chris McCormick <chris@mccormick.cx>
2023-04-24 13:07:25 +0800

    Loading mod + metadata with chiptune2.js cloned fns.

--- src/algotracker/ui.cljs
+++ src/algotracker/ui.cljs
@@ -23,15 +23,37 @@
                                                              (js/libopenmpt.ccall "openmpt_get_string" "string" #js ["string"] #js ["core_version"]))
                                              (res js/libopenmpt))}))))
 
+(defn load-mod [mod-data]
+  (let [barr (js/Int8Array. mod-data)
+        ptr-to-file (._malloc js/libopenmpt (j/get barr :length))]
+    (-> js/libopenmpt .-HEAPU8 (.set barr ptr-to-file))
+    (js/libopenmpt._openmpt_module_create_from_memory ptr-to-file (j/get barr :length) 0 0 0)))
+
+(defn get-metadata [mod-ptr]
+  (let [data #js {}
+        mod-keys (.split (js/UTF8ToString (._openmpt_module_get_metadata_keys js/libopenmpt mod-ptr)) #";")]
+    (doseq [k mod-keys]
+      (let [key-name-buffer (._malloc js/libopenmpt (+ (.-length k) 1))]
+        (js/writeAsciiToMemory k key-name-buffer)
+        (j/assoc! data k (js/UTF8ToString (._openmpt_module_get_metadata js/libopenmpt mod-ptr key-name-buffer)))
+        (._free js/libopenmpt key-name-buffer)))
+    data))
+
 (defn component-main [_state]
-  (let [mod-data (-> (rc/inline "small.mod.json")
+  (let [mod-json (rc/inline "small.mod.json")
+        mod-data (->
+                   mod-json
                    load-json
                    save-mod)
-        mod-file (js/File. #js [mod-data] #js {:name "test.mod" :content-type "audio/x-mod"})]
+        mod-file (js/File. #js [mod-data] #js {:name "small.mod" :content-type "audio/x-mod"})
+        module (load-mod mod-data)
+        metadata (get-metadata module)]
     [:div
      [:h1 "algotracker"]
      [:p "Algorithmically generating tracker music."]
-     [:a {:href (js/URL.createObjectURL mod-file) :download "small.mod"} "small.mod"]]))
+     [:a {:href (js/URL.createObjectURL mod-file) :download "small.mod"} "small.mod"]
+     [:h3 "small.mod"]
+     [:pre (js/JSON.stringify metadata nil 2)]]))
 
 (defn start {:dev/after-load true} []
   (p/let [res (.then mpt-promise)]
