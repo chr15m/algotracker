c3ffe125070594c241522ceefafd7e29834f0a97
Chris McCormick <chris@mccormick.cx>
2023-04-25 09:42:31 +0800

    Compute duration and render song to f32 buffers.

--- src/algotracker/ui.cljs
+++ src/algotracker/ui.cljs
@@ -39,26 +39,51 @@
         (._free js/libopenmpt key-name-buffer)))
     data))
 
-(defn component-main [_state]
-  (let [mod-json (rc/inline "small.mod.json")
-        mod-data (->
-                   mod-json
-                   load-json
-                   save-mod)
-        mod-file (js/File. #js [mod-data] #js {:name "small.mod" :content-type "audio/x-mod"})
-        module (load-mod mod-data)
-        metadata (get-metadata module)]
+(defn get-duration [mod-ptr]
+  (.toFixed (._openmpt_module_get_duration_seconds js/libopenmpt mod-ptr) 2))
+
+(defn render-song [mod-ptr]
+  (.time js/console "render-song")
+  (let [sample-rate 44100
+        song-duration-seconds (get-duration mod-ptr)
+        ;max-frames-per-chunk 4096
+        frame-count (* sample-rate song-duration-seconds)
+        mptbuffer-ptrs (map (fn [_i] (._malloc js/libopenmpt (* 4 frame-count))) (range 2))
+        actual-frames (._openmpt_module_read_float_stereo js/libopenmpt mod-ptr 44100 frame-count (first mptbuffer-ptrs) (second mptbuffer-ptrs))
+        raw-audio-arrays (map (fn [i]
+                                (let [buffer-ptr (nth mptbuffer-ptrs i)
+                                      idx (/ buffer-ptr 4)]
+                                  (-> js/libopenmpt .-HEAPF32 (.subarray idx (+ idx actual-frames)))))
+                              (range 2))]
+    (.timeEnd js/console "render-song")
+    raw-audio-arrays))
+
+(defn component-main [_state mod-file metadata duration]
+  (let []
     [:div
      [:h1 "ÄlgoTracker"]
      [:p "Algorithmic tracker music generator. It does not do anything yet."]
-     [:h3 "small.mod"]
+     [:h3 "small.mod (" duration "s)"]
      [:pre (js/JSON.stringify metadata nil 2)]
      [:p "download " [:a {:href (js/URL.createObjectURL mod-file) :download "small.mod"} "small.mod"]]]))
 
 (defn start {:dev/after-load true} []
-  (p/let [res (.then mpt-promise)]
-    (js/console.log "libopenmpt resolved:" res)
-    (rdom/render [component-main state]
+  (p/let [res (.then mpt-promise)
+          _ (js/console.log "libopenmpt loaded:" res)
+          mod-json (rc/inline "small.mod.json")
+          mod-data (->
+                     mod-json
+                     load-json
+                     save-mod)
+          mod-file (js/File. #js [mod-data] #js {:name "small.mod" :content-type "audio/x-mod"})
+          module (load-mod mod-data)
+          metadata (get-metadata module)
+          duration (get-duration module)
+          rendered (render-song module)]
+    ; clean up
+    (._openmpt_module_destroy js/libopenmpt module)
+    (js/console.log "raw-audio" (clj->js rendered))
+    (rdom/render [component-main state mod-file metadata duration]
                  (js/document.getElementById "app"))))
 
 (defn main! []
