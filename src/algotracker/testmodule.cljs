adf4fc3ec3b798ab65523ecfbd6acf9752065be7
Chris McCormick <chris@mccormick.cx>
2023-07-28 11:28:05 +0800

    Experimenting with sine waves.

--- src/algotracker/testmodule.cljs
+++ src/algotracker/testmodule.cljs
@@ -9,11 +9,14 @@
 (defn make-sample-set [_state]
   ; Return an array of audio file data
   (map
-    (fn [_i]
+    (fn [s]
       {:volume 1
        :name ""
-       :samples (map (fn [_i] (- (js/Math.random) 0.5)) (range 441))
-       ;:samples (map (fn [i] (js/Math.sin (/ i 4410))) (range 441))
+       ;:samples (map (fn [_i] (- (js/Math.random) 0.5)) (range 441))
+       :samples (map
+                  (fn [i] (js/Math.sin
+                            (* (/ i 441) js/Math.PI 10 (inc s))))
+                  (range 441))
        ;:loopStart 0
        ;:loopLength 441
        :wave ""
@@ -29,10 +32,10 @@
   ; samples indexed from 1 not 0
   [(for [i (range 64)]
      (when (= (* (mod i 4) (mod (inc i) 1)) 0)
-       {:note :E-6 :semitone 60 :sample 1 :fx 0x00}))
+       {:note (rand-nth [:E-6 :G-6 :F-5]) :semitone (rand-nth [60 72 65]) :sample 1 :fx 0xEC4}))
    (for [i (range 64)]
      (when (= (* (mod i 7) (mod i 5)) 0)
-       {:note :F#5 :semitone 60 :sample 2 :fx 0x00}))])
+       {:note (rand-nth [:F#5 :G-6]) :semitone (rand-nth [48 60 65 67]) :sample 2 :fx 0xEC4}))])
 
 (def export
   #js {:ui ui
